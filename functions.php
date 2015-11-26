df98da5fb884b6b32114735cf0fc07a863f28dbf
Ben Saufley <bensaufley@gmail.com>
2015-11-25 21:21:14 -0500

    Fleshed out tags/categories

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php
 
+add_theme_support( 'post-formats', array( 'aside', 'link', 'image', 'quote', 'video' ) );
+remove_action('wp_head', 'wp_generator');
+
 require_once(dirname(__FILE__).'/lib/Chester/require.php');
 
 ?>

a42fd38fcfc7354e63c82b43bbba03d783a8f9c0
Ben Saufley <bensaufley@gmail.com>
2015-11-25 20:44:48 -0500

    Initial commit with basic Chester outline

---
+++ mvc/controllers/site_controller.php
@@ -0,0 +1,20 @@
+<?php
+
+class SiteController extends ChesterBaseController {
+
+  public function showPostPreviews() {
+
+    $posts = ChesterWPCoreDataHelpers::getWordpressPostsFromLoop();
+
+    //we echo out the results of the renderPage function which outputs the content along with the header and footer
+    $this->renderPage('post_previews', array(
+      'posts' => $posts,
+      'next_posts_link' => get_next_posts_link(),
+      'previous_posts_link' => get_previous_posts_link(),
+    ));
+
+  }
+
+}
+
+?>

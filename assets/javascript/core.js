19aee8a397afacdd55cbea780cb3ab46e41696db
Ben Saufley <bensaufley@gmail.com>
2015-11-26 00:00:36 -0500

    Trying to get Babel working

---
+++ assets/javascript/core.js
@@ -0,0 +1,12 @@
+(function (window, document, undefined) {
+  const jQueryScript = document.createElement('script');
+
+  let initialize = function (test = 'thing') {
+    console.log(test);
+  };
+
+  jQueryScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js';
+  jQueryScript.type = 'text/javascript';
+  jQueryScript.onload = initialize;
+  document.body.appendChild(jQueryScript);
+})(window, document, undefined);
